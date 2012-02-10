3e756a3f1b95013824794591384677bf3e586d0a
Sooraj <soorajsooriadas@gmail.com>
2012-02-10 22:36:48 +0530

    Led blinking using LFSR

---
+++ lfsr.c
@@ -0,0 +1,24 @@
+#include<msp430.h>
+#include<stdint.h>
+void delay(void);
+main()
+{
+unsigned lfsr=0xace1;
+unsigned bit;
+P1DIR=0x40;
+P1OUT=0x40;
+do
+{
+
+bit = ((lfsr>>0)^(lfsr>>2)^(lfsr>>3)^(lfsr>>5))&1;
+lfsr=(lfsr>>1)|(bit<<15);
+P1OUT=lfsr;
+delay();
+}while(lfsr !=0xace1);
+}
+void delay()
+{
+unsigned int i;
+for(i=0;i<20000;i++);
+}
+
